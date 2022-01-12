d59f289f2c7f757e88b2dfb8aa96a28cb6c4ae24
giorgiadarmi <darmiento.1711864@studenti.uniroma1.it>
2022-01-12 14:44:01 +0100

    aggiunto maps

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,10 +1,6 @@
 class Ad < ApplicationRecord
     belongs_to :user
 
-   
-    def google_map(center)
-        "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=500x500&zoom=17&key=[YOUR_API_KEY]"
-    end
     def address
         [street, city, state, country].compact.join(', ')
     end
