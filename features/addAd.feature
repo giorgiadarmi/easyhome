aecb53f79214cfc01041a04aba67df4aa58f0f05
giorgiadarmi <darmiento.1711864@studenti.uniroma1.it>
2022-01-22 13:52:56 +0100

    aggiunto test aggiungi ad

--- features/addAd.feature
+++ features/addAd.feature
@@ -1,10 +1,11 @@
 Feature: User can manually add a ads
 
 Scenario: Add a articles
+	Given I am authenticated
 	Given I am on app homepage
 	When I follow "Crea Annuncio"
 	Then I should be on the Create New Ad Page
-	When I fill in "title" with "Attico in centro"
+	When I fill in "Titolo" with "Mansarda"
 	And I press "Save Changes"
 	Then I should be on the app homepage
-	And I should see "Attico in centro"
+	And I should see "Mansarda"
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,10 +1,20 @@
+Given('I am authenticated') do
+	u = User.create!(:email=>"easyhome0102@gmail.com",
+	:password => "easyhome.0102",
+	:roles_mask => 0 )
+	visit "/"
+	fill_in "Email", with: "easyhome0102@gmail.com"
+	fill_in "Password", with: "easyhome.0102"
+	click_button "Log in"
+end
+
 Given('I am on app homepage') do
     visit 'ads' 
     #pending # Write code here that turns the phrase above into concrete actions
 end
 
 When('I follow {string}') do |string|
-    click_link(string)
+	click_link(string)
     #pending # Write code here that turns the phrase above into concrete actions
 end
 
@@ -15,7 +25,7 @@ Then('I should be on the Create New Ad Page') do
 end
 
 When('I fill in {string} with {string}') do |field, value|
-    fill_in(field, :with => value)
+	fill_in(field, :with => value)
     #pending # Write code here that turns the phrase above into concrete actions
 end
   
